2571558b72aaa8039ab4224489f15b419fb180b3
travistn <travisn714@gmail.com>
2023-06-26 18:57:49 -0700

    Initialized a new list route that sends a POST request to create a new list object

---
+++ app/api/list/new/route.ts
@@ -0,0 +1,18 @@
+import { connectToDb } from '@/utils/database';
+import List from '@/models/list';
+
+export const POST = async (req: Request) => {
+  const { title } = await req.json();
+
+  try {
+    await connectToDb();
+    const newList = new List({ title });
+
+    await newList.save();
+
+    return new Response(JSON.stringify(newList), { status: 201 });
+  } catch (error) {
+    console.log(error);
+    return new Response('Failed to create a new list.', { status: 500 });
+  }
+};
