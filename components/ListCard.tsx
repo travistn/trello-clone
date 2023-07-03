1d8af8aab1f5bcf589c1c35711696dc0ee205786
travistn <travisn714@gmail.com>
2023-07-03 16:21:35 -0700

    Implemented createTask function to create a new task on button click

--- components/ListCard.tsx
+++ components/ListCard.tsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/solid';
 
 import TaskCard from './TaskCard';
+import CustomButton from './CustomButton';
+import Form from './Form';
 import { ListProps } from '@/types';
 
 interface ListCardProps {
@@ -9,6 +12,9 @@ interface ListCardProps {
 }
 
 const ListCard = ({ list, setIsSubmitted }: ListCardProps) => {
+  const [toggleAddTask, setToggleAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
   const handleDelete = async () => {
     const hasConfirmed = confirm('Are you sure you want to delete this list?');
 
@@ -27,6 +33,20 @@ const ListCard = ({ list, setIsSubmitted }: ListCardProps) => {
     }
   };
 
+  const createTask = async () => {
+    try {
+      await fetch('/api/task/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          description: 'Wash the car',
+          list: list._id,
+        }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='bg-[#f1f2f4] w-[272px] h-max flex flex-col gap-3 rounded-[12px] p-2'>
       <header className='flex flex-row justify-between items-center pl-3 pr-1'>
@@ -36,7 +56,25 @@ const ListCard = ({ list, setIsSubmitted }: ListCardProps) => {
           onClick={handleDelete}
         />
       </header>
+      <button onClick={createTask}>Add Task</button>
       {/* <TaskCard task='Wash the car' /> */}
+      {/* {!toggleAddTask ? (
+        <CustomButton
+          title='Add a task'
+          containerStyles='w-full px-2 py-1 rounded-[8px] hover:bg-gray-300'
+          textStyles='text-light-navy text-[14px]'
+          btnType='button'
+          plusIcon={true}
+          plusIconStyles='fill-light-navy stroke-[0.5] stroke-light-navy'
+          handleClick={() => setToggleAddTask((prevState) => !prevState)}
+        />
+      ) : (
+        <Form
+          placeholder='Enter a task...'
+          btnTitle='Add task'
+          handleCloseClick={() => setToggleAddTask((prevState) => !prevState)}
+        />
+      )} */}
     </div>
   );
 };
