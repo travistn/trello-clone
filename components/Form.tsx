71d1d8d94146e5fba1a1d8c538065310e9ddeebc
travistn <travisn714@gmail.com>
2023-07-05 16:13:54 -0700

    Edited Form to be able to submit either a list or a task

--- components/Form.tsx
+++ components/Form.tsx
@@ -10,25 +10,38 @@ const Form = ({
   list,
   setList,
   handleSubmit,
+  task,
+  setTask,
 }: FormProps) => {
   return (
     <form className='w-full flex flex-col gap-2' onSubmit={handleSubmit}>
-      <input
-        autoFocus
-        placeholder={placeholder}
-        value={list?.title}
-        onChange={(e) => setList?.({ title: e.target.value })}
-        className={`pl-2 py-1 h-[32px] outline-[#1D7AFC] text-[14px] placeholder:text-light-navy`}
-      />
+      {list && (
+        <input
+          autoFocus
+          placeholder={placeholder}
+          value={list?.title}
+          onChange={(e) => setList?.({ title: e.target.value })}
+          className='pl-2 py-1 h-[32px] outline-[#1D7AFC] text-[14px] placeholder:text-light-navy'
+        />
+      )}
+      {task && (
+        <input
+          autoFocus
+          placeholder={placeholder}
+          value={task?.description}
+          onChange={(e) => setTask?.({ description: e.target.value })}
+          className='pl-2 py-1 h-[32px] outline-[#1D7AFC] text-[14px] placeholder:text-light-navy rounded-[8px]'
+        />
+      )}
       <div className='flex flex-row gap-2'>
         <CustomButton
           title={btnTitle}
-          containerStyles='bg-[#0c66e4] px-3 py-1.5 rounded-md hover:bg-[#0055CC]'
+          containerStyles='bg-[#0c66e4] px-3 py-1.5 rounded-[4px] hover:bg-[#0055CC]'
           textStyles='text-white text-[14px]'
           btnType='submit'
         />
         <XMarkIcon
-          className='w-[24px] fill-light-navy stroke-[0.2] stroke-light-navy cursor-pointer'
+          className='w-[24px] fill-light-navy stroke-[0.2] stroke-light-navy cursor-pointer hover:fill-dark-navy'
           onClick={handleCloseClick}
         />
       </div>
