40687af7f15e496e0b3784dab388ebb1fb35bf3e
travistn <travisn714@gmail.com>
2023-07-05 16:13:11 -0700

    Updated TaskCard styles

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -1,9 +1,13 @@
-import { TaskCardProps } from '@/types';
+import { TaskProps } from '@/types';
+
+interface TaskCardProps {
+  task: TaskProps;
+}
 
 const TaskCard = ({ task }: TaskCardProps) => {
   return (
-    <div className='bg-white rounded-[12px] p-2 w-full h-fit'>
-      <p className='text-[14px] text-navy'>{task}</p>
+    <div className='bg-white rounded-[8px] p-2 w-full h-fit shadow-md cursor-pointer hover:bg-gray-200'>
+      <p className='text-[14px] text-navy'>{task.description}</p>
     </div>
   );
 };
