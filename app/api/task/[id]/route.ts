81bf9698f770176a78f16000b7a85058ebcc01b0
travistn <travisn714@gmail.com>
2023-07-20 20:30:43 -0700

    List ID in Task object will change ID's if a new list ID is passed in

--- app/api/task/[id]/route.ts
+++ app/api/task/[id]/route.ts
@@ -4,20 +4,22 @@ import { connectToDb } from '@/utils/database';
 import Task from '@/models/task';
 
 export const PATCH = async (req: Request, { params }: { params: { id: string } }) => {
-  const { description } = await req.json();
+  const { description, list, category } = await req.json();
 
   try {
     await connectToDb();
 
     const existingTask = await Task.findById(params.id);
 
-    existingTask.description = description;
+    if (category === 'description') existingTask.description = description;
+
+    if (category === 'list') existingTask.list = list;
 
     await existingTask.save();
 
-    return new Response('Successfully updated the task', { status: 200 });
+    return NextResponse.json('Successfully updated the task', { status: 200 });
   } catch (error) {
-    return new Response(JSON.stringify('Failed to update the task.'), {
+    return NextResponse.json(JSON.stringify('Failed to update the task.'), {
       status: 500,
     });
   }
