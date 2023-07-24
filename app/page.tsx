453a28789c21ab4e8fb573c4c03a3ea6c873f83c
travistn <travisn714@gmail.com>
2023-07-24 01:19:51 -0700

    Edited styling for mobile background to display all of screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Board from '@/components/Board';
 
 const Home = () => {
   return (
-    <main className='gradient max-sm:w-full max-sm:h-full md:h-screen md:w-screen'>
+    <main className='gradient max-sm:w-full max-sm:min-h-screen md:h-screen md:w-screen'>
       <Board />
     </main>
   );
