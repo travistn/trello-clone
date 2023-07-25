3827335c306eef7a9347bf4327718f70fe9030eb
travistn <travisn714@gmail.com>
2023-07-25 16:30:37 -0700

    Background height will expand on mobile when divs appear on DOM

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Board from '@/components/Board';
 
 const Home = () => {
   return (
-    <main className='gradient max-sm:w-full max-sm:min-h-screen md:h-screen md:w-screen'>
+    <main className='gradient max-sm:w-full max-sm:min-h-screen md:h-screen md:w-screen max-sm:overflow-auto'>
       <Board />
     </main>
   );
