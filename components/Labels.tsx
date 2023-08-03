1576dc05eace2ba80adcb9efd6c1e00b1cb9b79c
travistn <travisn714@gmail.com>
2023-08-03 15:04:55 -0700

    Defined fetchLabels to retrieve all labels from MongoDB to be displayed

--- components/Labels.tsx
+++ components/Labels.tsx
@@ -1,10 +1,26 @@
+import { useState, useEffect } from 'react';
 import { XMarkIcon, PencilIcon } from '@heroicons/react/24/solid';
+
+import { LabelProps } from '@/types';
 
 interface LabelsProps {
   setOpenLabels: (toggle: boolean) => void;
 }
 
 const Labels = ({ setOpenLabels }: LabelsProps) => {
+  const [labels, setLabels] = useState([]);
+
+  useEffect(() => {
+    const fetchLabels = async () => {
+      const response = await fetch('/api/label');
+      const data = await response.json();
+
+      setLabels(data);
+    };
+
+    fetchLabels();
+  }, []);
+
   return (
     <div className='p-3 bg-white rounded-[8px] w-[300px] cursor-default'>
       <header className='flex flex-row items-center'>
@@ -16,21 +32,16 @@ const Labels = ({ setOpenLabels }: LabelsProps) => {
           />
         </div>
       </header>
-      <div className='flex flex-col gap-1 mt-2'>
-        <div className='flex flex-row items-center justify-between gap-2 pl-2'>
-          <input type='checkbox' className='w-4 h-4 cursor-pointer' />
-          <div className='w-[210px] h-[30px] bg-green rounded-[3px]' />
-          <div className='rounded-[3px] p-2 cursor-pointer hover:bg-gray-300'>
-            <PencilIcon className='w-[11px] fill-white stroke-light-navy stroke-2' />
+      <div className='flex flex-col gap-2 mt-2'>
+        {labels.map((label: LabelProps) => (
+          <div key={label._id} className='flex flex-row items-center justify-between gap-2 pl-2'>
+            <input type='checkbox' className='w-4 h-4 cursor-pointer' />
+            <div className={`w-[210px] h-[30px] bg-${label.color} rounded-[3px]`} />
+            <div className='rounded-[3px] p-2 cursor-pointer hover:bg-gray-300'>
+              <PencilIcon className='w-[11px] fill-white stroke-light-navy stroke-2' />
+            </div>
           </div>
-        </div>
-        <div className='flex flex-row items-center justify-between gap-2 pl-2'>
-          <input type='checkbox' className='w-4 h-4 cursor-pointer' />
-          <div className='w-[210px] h-[30px] bg-yellow rounded-[3px]' />
-          <div className='rounded-[3px] p-2 cursor-pointer hover:bg-gray-300'>
-            <PencilIcon className='w-[11px] fill-white stroke-light-navy stroke-2' />
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
