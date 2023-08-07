c0b19676e0e26489eb780aed64f99adabdec4d9a
travistn <travisn714@gmail.com>
2023-08-07 14:17:30 -0700

    Labels will be shown in textarea when adding or removing labels

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -66,15 +66,27 @@ const TaskCard = ({ task, setIsSubmitted }: TaskCardProps) => {
       {toggleEdit && (
         <div className='flex flex-col w-full absolute top-0 left-0 z-10'>
           <div ref={taskRef}>
-            <textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              spellCheck={false}
-              className='text-[14px] text-navy p-3 resize-none outline-none h-[90px] w-full rounded-[8px] leading-5'
-            />
+            <div className='flex flex-col p-2 bg-white rounded-[8px] '>
+              {(task?.labels?.length as number) > 0 && (
+                <div className='flex flex-row flex-wrap gap-1 pl-1 mb-1'>
+                  {task?.labels?.map((label) => (
+                    <div
+                      key={label._id}
+                      className={`h-[6.5px] min-w-[40px] max-w-[40px] rounded-[8px] bg-${label.color}`}
+                    />
+                  ))}
+                </div>
+              )}
+              <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                spellCheck={false}
+                className='text-[14px] p-1 text-navy resize-none outline-none h-[80px] w-full rounded-[8px] leading-5'
+              />
+            </div>
             <CustomButton
               title='Save'
-              containerStyles='w-fit bg-[#0c66e4] px-4 py-1.5 rounded-[4px] hover:bg-[#0055CC]'
+              containerStyles='w-fit bg-[#0c66e4] px-4 py-1.5 rounded-[4px] mt-1.5 hover:bg-[#0055CC]'
               textStyles='text-[14px] text-white'
               btnType='submit'
               handleClick={updateTaskDescription}
