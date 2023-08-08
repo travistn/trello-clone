b803bb4f0a30304054043e6cf4f6179a3164980c
travistn <travisn714@gmail.com>
2023-08-08 14:11:44 -0700

    Label title will be displayed in the label color container

--- components/LabelCard.tsx
+++ components/LabelCard.tsx
@@ -58,9 +58,10 @@ const LabelCard = ({
         className='w-4 h-4 cursor-pointer'
       />
       <div
-        className={`w-[210px] h-[30px] bg-${label.color} rounded-[3px] cursor-pointer hover:brightness-[.80]`}
-        onClick={handleLabelCheck}
-      />
+        className={`w-[210px] h-[32px] bg-${label.color} rounded-[3px] cursor-pointer text-[14px] font-medium pl-3 flex items-center text-ellipsis whitespace-nowrap hover:brightness-[.80]`}
+        onClick={handleLabelCheck}>
+        {label.title}
+      </div>
       <div className='rounded-[3px] p-2 cursor-pointer hover:bg-gray-300'>
         <PencilIcon
           className='w-[11px] fill-white stroke-light-navy stroke-2'
