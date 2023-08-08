f39b83a0ef67f9776a881357fc125a1aa5d61416
travistn <travisn714@gmail.com>
2023-08-08 13:49:33 -0700

    Defined editLabel function to create or edit a Label title

--- components/Labels.tsx
+++ components/Labels.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, FormEvent } from 'react';
 import { XMarkIcon, ChevronLeftIcon } from '@heroicons/react/24/solid';
 
 import { LabelProps, TaskProps } from '@/types';
@@ -15,8 +15,25 @@ const Labels = ({ task, setOpenLabels, setIsSubmitted }: LabelsProps) => {
   const [labels, setLabels] = useState([]);
   const [selectedLabel, setSelectedLabel] = useState<LabelProps>();
 
-  const [labelTitle, setLabelTitle] = useState({ title: '' });
+  const [labelTitle, setLabelTitle] = useState('');
   const [toggleEditLabel, setToggleEditLabel] = useState(false);
+
+  const editLabel = async () => {
+    try {
+      await fetch(`/api/label/${selectedLabel?._id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          label: selectedLabel,
+          title: labelTitle,
+          action: 'editLabel',
+        }),
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setToggleEditLabel(false);
+    }
+  };
 
   useEffect(() => {
     const fetchLabels = async () => {
@@ -27,7 +44,7 @@ const Labels = ({ task, setOpenLabels, setIsSubmitted }: LabelsProps) => {
     };
 
     fetchLabels();
-  }, []);
+  }, [toggleEditLabel]);
 
   return (
     <div className='bg-white rounded-[8px] w-[300px] cursor-default'>
@@ -68,20 +85,21 @@ const Labels = ({ task, setOpenLabels, setIsSubmitted }: LabelsProps) => {
           <div className='bg-[#f7f8f9] p-8 flex justify-center items-center'>
             <div className={`w-[230px] h-[30px] bg-${selectedLabel?.color} rounded-[3px]`} />
           </div>
-          <form className='flex flex-col gap-1 p-3 w-full'>
+          <div className='flex flex-col gap-1 p-3 w-full'>
             <label className='text-[12px] text-light-navy font-semibold'>Title</label>
             <input
               autoFocus
-              onChange={(e) => setLabelTitle({ title: e.target.value })}
+              onChange={(e) => setLabelTitle(e.target.value)}
               className='text-[14px] text-navy px-2 py-1.5 border-2 border-[#dcdfe4] rounded-[3px] focus:outline-[#1D7AFC]'
             />
             <CustomButton
               title='Save'
               containerStyles='w-fit bg-[#0c66e4] px-4 py-1.5 rounded-[3px] mt-1.5 hover:bg-[#0055CC]'
               textStyles='text-[14px] text-white'
-              btnType='submit'
+              btnType='button'
+              handleClick={editLabel}
             />
-          </form>
+          </div>
         </div>
       )}
     </div>
