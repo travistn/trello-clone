29e9c818b38908ad5f3d0e8527f552d097a17835
travistn <travisn714@gmail.com>
2023-08-10 15:53:30 -0700

    Added a width class so that Tooltip extends outside of container div if necessary

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -6,7 +6,7 @@ interface TooltipProps {
 
 const Tooltip = ({ label }: TooltipProps) => {
   return (
-    <div className='text-[12px] text-white bg-light-navy rounded-[3px] px-1.5 py-[1px] absolute mt-2 hidden group-hover/label:flex'>
+    <div className='min-w-max text-[12px] text-white bg-light-navy rounded-[3px] px-1.5 py-[1px] absolute mt-2 hidden group-hover/label:flex'>
       {`Color: ${label.color}, title: ${label.title !== '' ? `"${label.title}"` : 'none'}`}
     </div>
   );
