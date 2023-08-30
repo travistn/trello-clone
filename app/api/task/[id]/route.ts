4fa48cd9df5d3d99aee7c8124a9dde26550b0a6a
travistn <travisn714@gmail.com>
2023-08-29 17:05:36 -0700

    Removed unused logic

--- app/api/task/[id]/route.ts
+++ app/api/task/[id]/route.ts
@@ -2,10 +2,9 @@ import { NextResponse } from 'next/server';
 
 import { connectToDb } from '@/utils/database';
 import Task from '@/models/task';
-import List from '@/models/list';
 
 export const PATCH = async (req: Request, { params }: { params: { id: string } }) => {
-  const { description, list, category, task, tasks, action, id } = await req.json();
+  const { description, task, category, action } = await req.json();
 
   try {
     await connectToDb();
@@ -13,35 +12,6 @@ export const PATCH = async (req: Request, { params }: { params: { id: string } }
     const existingTask = await Task.findById(params.id);
 
     if (category === 'description') existingTask.description = description;
-
-    if (category === 'list') {
-      existingTask.list = list._id;
-
-      const removeTaskFromList = await List.findOneAndUpdate(
-        { _id: task.list },
-        {
-          $pull: {
-            tasks: task._id,
-          },
-        },
-        { safe: true }
-      );
-
-      const existingList = await List.findById(list._id);
-
-      existingList.tasks = tasks;
-
-      await existingList.save();
-      await removeTaskFromList.save();
-    }
-
-    if (category === 'position') {
-      const existingList = await List.findById(list._id);
-
-      existingList.tasks = tasks;
-
-      await existingList.save();
-    }
 
     if (action === 'updateTaskOrder') {
       existingTask.order = task.order;
