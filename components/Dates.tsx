e294e08fed191b98791334b195f4b890dab3733a
travistn <travisn714@gmail.com>
2023-09-05 15:28:35 -0700

    Created inputs that will display the selected day's date and time

---
+++ components/Dates.tsx
@@ -0,0 +1,102 @@
+import { ChangeEventHandler, useState } from 'react';
+import { DateFormatter, DayPicker, useInput } from 'react-day-picker';
+import { format, isValid, parse } from 'date-fns';
+import { XMarkIcon } from '@heroicons/react/24/solid';
+
+import 'react-day-picker/dist/style.css';
+import '/styles/day-picker.css';
+import CustomCaption from './CustomCaption';
+
+interface DateProps {
+  setOpenDate: (toggle: boolean) => void;
+}
+
+const Dates = ({ setOpenDate }: DateProps) => {
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>();
+  const [dayInputValue, setDayInputValue] = useState<string>('');
+  const [timeInputValue, setTimeInputValue] = useState<string>('');
+  const [checked, setChecked] = useState(true);
+
+  const formatWeekdayName: DateFormatter = (day, options) => {
+    return format(day, 'EEE', { locale: options?.locale });
+  };
+
+  const handleDaySelect = (date: Date | undefined) => {
+    setSelectedDate(date);
+
+    if (date) {
+      setDayInputValue(format(date, 'M/d/Y'));
+      setTimeInputValue(format(date, 'p'));
+    }
+  };
+
+  const handleDateChecked = () => {};
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const date = parse(e.currentTarget.value, 'MM/dd/yyyy HH:mm', new Date());
+
+    if (isValid(date)) {
+      setSelectedDate(date);
+    } else {
+      setSelectedDate(undefined);
+    }
+  };
+
+  return (
+    <div className='bg-white rounded-[8px] w-[300px] cursor-default flex flex-col p-3 gap-2'>
+      <header className='grid grid-cols-3 justify-items-center items-center'>
+        <h3 className='text-[14px] text-light-navy font-semibold col-start-2'>Dates</h3>
+        <div
+          className='rounded-md p-1.5 ml-auto hover:bg-gray-300 hover:cursor-pointer'
+          onClick={() => setOpenDate(false)}>
+          <XMarkIcon className='w-[15px] text-light-navy stroke-light-navy stroke-[0.5]' />
+        </div>
+      </header>
+      <div className='flex flex-col items-center'>
+        <DayPicker
+          mode='single'
+          selected={selectedDate}
+          onSelect={handleDaySelect}
+          showOutsideDays
+          formatters={{ formatWeekdayName }}
+          components={{ Caption: CustomCaption }}
+          onDayClick={() => setChecked(true)}
+        />
+      </div>
+      <div className='flex flex-col gap-2'>
+        <label className='text-[12px] text-[#0c66e4] font-bold leading-4'>Due date</label>
+        <div className='flex flex-row items-center gap-2'>
+          <input
+            type='checkbox'
+            className='cursor-pointer w-4 h-4'
+            checked={checked}
+            onChange={handleDateChecked}
+            onClick={() => setChecked((prevState) => !prevState)}
+          />
+          <input
+            type='text'
+            placeholder='M/D/YYYY'
+            disabled={!checked}
+            value={dayInputValue}
+            onChange={handleInputChange}
+            className={`w-[90px] text-[14px] leading-5 rounded-[3px] p-1.5 text-light-navy bg-[#fafafa] ${
+              checked ? 'outline-[#0c66e4]' : 'cursor-not-allowed	'
+            }`}
+          />
+          <input
+            type='text'
+            placeholder='h:mm A'
+            disabled={!checked}
+            value={timeInputValue}
+            onChange={handleInputChange}
+            className={`w-[90px] text-[14px] leading-5 rounded-[3px] p-1.5 text-light-navy bg-[#fafafa] ${
+              checked ? 'outline-[#0c66e4]' : 'cursor-not-allowed	'
+            }`}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dates;
