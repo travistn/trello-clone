040e3cb001227f59454af356338f7d4667bcd58f
travistn <travisn714@gmail.com>
2023-09-12 16:40:21 -0700

    Added styles for when a task is overdue and when a task is due in less than 24 hours

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -4,6 +4,7 @@ import {
   DraggableProvidedDraggableProps,
 } from 'react-beautiful-dnd';
 import { format } from 'date-fns';
+import moment from 'moment';
 import { PencilIcon, ClockIcon } from '@heroicons/react/24/solid';
 
 import { TaskProps } from '@/types';
@@ -34,6 +35,24 @@ const TaskCard = ({
   const taskRef = useRef(null);
 
   useClickoutClose(taskRef, setToggleEdit);
+
+  const getDueStyles = (dueDate: Date, element: string) => {
+    const currentDate = new Date();
+
+    if (currentDate > dueDate) {
+      if (element === 'div') return 'bg-[#ffedeb] hover:bg-[#ffd2cc]';
+      if (element === 'icon') return 'fill-[#e34935] stroke-white';
+      if (element === 'p') return 'text-[#ae2a19]';
+    } else if (moment(task.dueDate).fromNow() === 'in a day') {
+      if (element === 'div') return 'bg-[#e2b203] hover:bg-[#cf9f02]';
+      if (element === 'icon') return 'fill-[#e2b203] stroke-light-navy';
+      if (element === 'p') return 'text-light-navy';
+    } else {
+      if (element === 'div') return 'hover:bg-[#091e420f]';
+      if (element === 'icon') return 'fill-white stroke-[#626f86]';
+      if (element === 'p') return 'text-light-navy';
+    }
+  };
 
   const updateTaskDescription = async () => {
     setIsSubmitted(true);
@@ -118,19 +137,27 @@ const TaskCard = ({
       {task.dueDate && (
         <div
           className={`w-fit flex flex-row items-center gap-2 px-1.5 py-1 rounded-[3px] cursor-pointer ${
-            task.isDue ? 'hover:bg-[#091e420f]' : 'bg-[#1f845a] hover:bg-[#216e4e]'
+            task.isDue
+              ? getDueStyles(new Date(task.dueDate), 'div')
+              : 'bg-[#1f845a] hover:bg-[#216e4e]'
           }`}
           onClick={updateTaskDue}>
           <ClockIcon
             className={`w-[16px] h-[16px] stroke-[1.5] ${
-              task.isDue ? 'fill-white stroke-[#626f86]' : 'fill-[#1f845a] stroke-white'
+              task.isDue
+                ? getDueStyles(new Date(task.dueDate), 'icon')
+                : 'fill-[#1f845a] stroke-white'
             }`}
           />
-          <p className={`text-[12px] ${task.isDue ? 'text-light-navy' : 'text-white'}`}>
+          <p
+            className={`text-[12px] ${
+              task.isDue ? getDueStyles(new Date(task.dueDate), 'p') : 'text-white'
+            }`}>
             {format(new Date(task.dueDate), 'MMM d')}
           </p>
         </div>
       )}
+
       {toggleEdit && (
         <div className='flex flex-col w-full absolute top-0 left-0 z-10'>
           <div ref={taskRef}>
@@ -158,15 +185,22 @@ const TaskCard = ({
               {task.dueDate && (
                 <div
                   className={`w-fit flex flex-row items-center gap-2 px-1.5 py-1 rounded-[3px] cursor-pointer ${
-                    task.isDue ? 'hover:bg-[#091e420f]' : 'bg-[#1f845a] hover:bg-[#216e4e]'
+                    task.isDue
+                      ? getDueStyles(new Date(task.dueDate), 'div')
+                      : 'bg-[#1f845a] hover:bg-[#216e4e]'
                   }`}
                   onClick={updateTaskDue}>
                   <ClockIcon
                     className={`w-[16px] h-[16px] stroke-[1.5] ${
-                      task.isDue ? 'fill-white stroke-[#626f86]' : 'fill-[#1f845a] stroke-white'
+                      task.isDue
+                        ? getDueStyles(new Date(task.dueDate), 'icon')
+                        : 'fill-[#1f845a] stroke-white'
                     }`}
                   />
-                  <p className={`text-[12px] ${task.isDue ? 'text-light-navy' : 'text-white'}`}>
+                  <p
+                    className={`text-[12px] ${
+                      task.isDue ? getDueStyles(new Date(task.dueDate), 'p') : 'text-white'
+                    }`}>
                     {format(new Date(task.dueDate), 'MMM d')}
                   </p>
                 </div>
