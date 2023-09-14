4849573bece8cdd71bc4b7b7ea6b4d50a7621628
travistn <travisn714@gmail.com>
2023-09-14 16:40:47 -0700

    Added a tooltip to due dates to notify whether a due date is completed or not

--- components/Due.tsx
+++ components/Due.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import moment from 'moment';
 import { format } from 'date-fns';
 import { ClockIcon } from '@heroicons/react/24/solid';
 
 import { TaskProps } from '@/types';
+import Tooltip from './Tooltip';
 
 interface DueProps {
   task: TaskProps;
@@ -12,18 +13,19 @@ interface DueProps {
 
 const Due = ({ task, setIsSubmitted }: DueProps) => {
   const [isDue, setIsDue] = useState<boolean>();
+  const [tooltipMessage, setTooltipMessage] = useState('');
 
   const getDueStyles = (dueDate: Date, element: string) => {
     const currentDate = new Date();
 
     if (currentDate > dueDate) {
-      if (element === 'div') return 'bg-[#ffedeb] hover:bg-[#ffd2cc]';
-      if (element === 'icon') return 'fill-[#e34935] stroke-white';
-      if (element === 'p') return 'text-[#ae2a19]';
+      if (element === 'div') return 'bg-[#ca3521] hover:bg-[#ae2a19]';
+      if (element === 'icon') return 'fill-[#ca3521] stroke-white';
+      if (element === 'p') return 'text-white';
     } else if (moment(task.dueDate).fromNow() === 'in a day') {
       if (element === 'div') return 'bg-[#e2b203] hover:bg-[#cf9f02]';
-      if (element === 'icon') return 'fill-[#e2b203] stroke-light-navy';
-      if (element === 'p') return 'text-light-navy';
+      if (element === 'icon') return 'fill-[#e2b203] stroke-white';
+      if (element === 'p') return 'text-white';
     } else {
       if (element === 'div') return 'hover:bg-[#091e420f]';
       if (element === 'icon') return 'fill-white stroke-[#626f86]';
@@ -49,9 +51,27 @@ const Due = ({ task, setIsSubmitted }: DueProps) => {
     }
   };
 
+  useEffect(() => {
+    const getTooltipMessage = (dueDate: Date) => {
+      const currentDate = new Date();
+
+      if (currentDate > dueDate && task.isDue === true) {
+        setTooltipMessage('This task is past due.');
+      } else if (moment(task.dueDate).fromNow() === 'in a day' && task.isDue === true) {
+        setTooltipMessage('This task is due in less than 24 hours.');
+      } else if (task.isDue === false) {
+        setTooltipMessage('This task is complete.');
+      } else {
+        setTooltipMessage('This task is due later.');
+      }
+    };
+
+    getTooltipMessage(new Date(task.dueDate as Date));
+  }, [task.isDue]);
+
   return (
     <div
-      className={`w-fit flex flex-row items-center gap-2 px-1.5 py-1 rounded-[3px] cursor-pointer ${
+      className={`w-fit flex flex-row items-center gap-2 px-1.5 py-1 rounded-[3px] cursor-pointer relative group/due ${
         task.isDue
           ? getDueStyles(new Date(task.dueDate as Date), 'div')
           : 'bg-[#1f845a] hover:bg-[#216e4e]'
@@ -70,6 +90,7 @@ const Due = ({ task, setIsSubmitted }: DueProps) => {
         }`}>
         {format(new Date(task.dueDate as Date), 'MMM d')}
       </p>
+      <Tooltip type='due' message={tooltipMessage} />
     </div>
   );
 };
