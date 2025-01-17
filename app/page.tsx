1407db6cc0ba5df4261f00382acc09d3c47effe3
travistn <travisn714@gmail.com>
2025-01-16 17:51:32 -0800

    Home page will either display login page or dashboard depending on whether a user has logged in or not

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+
 import Login from '@/components/Login';
+import Board from '@/components/Board';
 
 const Home = () => {
+  const { data: session } = useSession();
+
   return (
     <main className='max-sm:w-full max-sm:min-h-screen md:h-screen md:w-screen max-sm:overflow-auto flex justify-center items-center bg-[#fafbfc]'>
-      <Login />
+      {session?.user ? <Board /> : <Login />}
     </main>
   );
 };
