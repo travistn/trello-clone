edaa4506f88e2dc1c4e24e7900f6aebc733eb60c
travistn <travisn714@gmail.com>
2025-01-16 19:12:20 -0800

    Insert Navbar component so it appears with Board component

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,24 @@ import { useSession } from 'next-auth/react';
 
 import Login from '@/components/Login';
 import Board from '@/components/Board';
+import Navbar from '@/components/Navbar';
 
 const Home = () => {
   const { data: session } = useSession();
 
   return (
-    <main className='max-sm:w-full max-sm:min-h-screen md:h-screen md:w-screen max-sm:overflow-auto flex justify-center items-center bg-[#fafbfc]'>
-      {session?.user ? <Board /> : <Login />}
+    <main
+      className={`max-sm:w-full max-sm:min-h-screen md:h-screen md:w-screen max-sm:overflow-auto ${
+        session?.user ? 'gradient' : 'bg-[#fafbfc] flex justify-center items-center'
+      }`}>
+      {session?.user ? (
+        <div>
+          <Navbar />
+          <Board />
+        </div>
+      ) : (
+        <Login />
+      )}
     </main>
   );
 };
