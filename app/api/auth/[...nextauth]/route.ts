a73108279ce592642f1ffb9f78ffd81930d1daaa
travistn <travisn714@gmail.com>
2025-01-21 15:09:08 -0800

    Implement handling for guest account creation

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { randomUUID } from 'crypto';
 
 import { connectToDb } from '@/utils/database';
 import User from '@/models/user';
@@ -10,30 +12,66 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+
+    CredentialsProvider({
+      name: 'Guest Login',
+      credentials: {},
+      async authorize(credentials, req) {
+        const guestId = randomUUID();
+
+        await User.create({
+          guestId,
+          accountType: 'guest',
+        });
+
+        const user = await User.findOne({ guestId });
+
+        if (user) return user;
+        else return null;
+      },
+    }),
   ],
   callbacks: {
-    async session({ session }: any) {
-      const sessionUser = await User.findOne({ email: session.user?.email });
-
-      session.user.id = sessionUser._id.toString();
-
-      return session;
+    async jwt({ token, user }) {
+      if (user) {
+        return { ...token, id: user.id };
+      }
+      return token;
     },
 
-    async signIn({ profile }: any) {
+    async session({ session, token }: any) {
+      if (session?.user.email) {
+        const sessionUser = await User.findOne({ email: session.user?.email });
+
+        session.user.id = sessionUser._id.toString();
+
+        return session;
+      } else {
+        return {
+          ...session,
+          user: {
+            id: token.id,
+          },
+        };
+      }
+    },
+
+    async signIn({ profile }) {
       try {
         await connectToDb();
 
-        const userExists = await User.findOne({
-          email: profile.email,
-        });
-
-        if (!userExists) {
-          await User.create({
+        if (profile) {
+          const userExists = await User.findOne({
             email: profile.email,
           });
-        }
 
+          if (!userExists) {
+            await User.create({
+              email: profile.email,
+              accountType: 'google',
+            });
+          }
+        }
         return true;
       } catch (error) {
         console.log(error);
