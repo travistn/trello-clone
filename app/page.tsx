fdf781d6d56270e8756e0f4a28992d31e24d6456
travistn <travisn714@gmail.com>
2025-01-23 15:52:37 -0800

    Remove Board component so that Login component only displays on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,9 @@
 import { useSession } from 'next-auth/react';
 
 import Login from '@/components/Login';
-import Board from '@/components/Board';
-import Navbar from '@/components/Navbar';
 import { useUserStore } from '@/store/store';
 import { UserProps } from '@/types';
+import { redirect } from 'next/navigation';
 
 const Home = () => {
   const { data: session } = useSession();
@@ -15,19 +14,13 @@ const Home = () => {
 
   setUser(session?.user as UserProps);
 
+  if (session) {
+    return redirect(`/board/${session?.user?.id}`);
+  }
+
   return (
-    <main
-      className={`max-sm:w-full max-sm:min-h-screen md:h-screen md:w-screen max-sm:overflow-auto ${
-        session?.user ? 'gradient' : 'bg-[#fafbfc] flex justify-center items-center'
-      }`}>
-      {session?.user ? (
-        <div>
-          <Navbar />
-          <Board />
-        </div>
-      ) : (
-        <Login />
-      )}
+    <main className='max-sm:w-full max-sm:min-h-screen md:h-screen md:w-screen max-sm:overflow-auto bg-[#fafbfc] flex justify-center items-center'>
+      <Login />
     </main>
   );
 };
